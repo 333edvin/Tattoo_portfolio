08b006859fb80d7ebfa94e4ebbfec510c14c6de0
johnsonerm <edvinjohnson333@gmail.com>
2024-02-01 16:06:10 +0530

    images updated

--- script.js
+++ script.js
@@ -2,110 +2,56 @@ document.addEventListener("DOMContentLoaded", function () {
     const galleryContainer = document.getElementById("image-gallery");
 
     const imagePaths = [ 
-        "./images/20220217_233811-01.jpeg.jpg",
-        "./images/IMG_20221125_235257.jpg",
-        "./images/IMG_20221126_214030.jpg",
-        "./images/IMG_20221129_190251.jpg",
-        "./images/IMG_20221130_194815.jpg",
-        "./images/IMG_20221201_150535.jpg",
-        "./images/IMG_20221202_000106.jpg",
-        "./images/IMG_20221202_221728.jpg",
-        "./images/IMG_20221203_151327.jpg",
-        "./images/IMG_20221203_151331.jpg",
-        "./images/IMG_20221203_215745.jpg",
-        "./images/IMG_20221204_023012.jpg",
-        "./images/IMG_20221204_023017.jpg",
-        "./images/IMG_20221204_132824.jpg",
-        "./images/IMG_20221204_205824.jpg",
-        "./images/IMG_20221207_162219.jpg",
-        "./images/IMG_20221207_222313.jpg",
-        "./images/IMG_20221207_222321.jpg",
-        "./images/IMG_20221208_202700.jpg",
-        "./images/IMG_20221209_150659.jpg",
-        "./images/IMG_20221210_151629.jpg",
-        "./images/IMG_20221210_195234.jpg",
-        "./images/IMG_20221211_184004.jpg",
-        "./images/IMG_20221211_230408.jpg",
-        "./images/IMG_20221211_230413.jpg",
-        "./images/IMG_20221212_152408.jpg",
-        "./images/IMG_20221217_134239.jpg",
-        "./images/IMG_20221217_192200.jpg",
-        "./images/IMG_20221225_061805-01.jpeg.jpg",
-        "./images/IMG_20221225_061805.jpg",
-        "./images/IMG_20221225_172506.jpg",
-        "./images/IMG_20221227_161949-01.jpeg.jpg",
-        "./images/IMG_20221229_194853-01.jpeg.jpg",
-        "./images/IMG_20221230_013323-01.jpeg.jpg",
-        "./images/IMG_20230107_223347.jpg",
-        "./images/IMG_20230107_223438-01.jpeg.jpg",
-        "./images/IMG_20230107_223509-01.jpeg.jpg",
-        "./images/IMG_20230108_173113-01.jpeg.jpg",
-        "./images/IMG_20230108_173332.jpg",
-        "./images/IMG_20230112_143335.jpg",
-        "./images/IMG_20230117_222915.jpg",
-        "./images/IMG_20230121_185408-01.jpeg.jpg",
-        "./images/IMG_20230128_175207-01-01.jpeg.jpg",
-        "./images/IMG_20230129_215540-01.jpeg.jpg",
-        "./images/IMG_20230204_181539.jpg",
-        "./images/IMG_20230206_164058.jpg",
-        "./images/IMG_20230206_164416.jpg",
-        "./images/IMG_20230212_160222.jpg",
-        "./images/IMG_20230212_190007.jpg",
-        "./images/IMG_20230219_181959.jpg",
-        "./images/IMG_20230222_145759.jpg",
-        "./images/IMG_20230227_164515.jpg",
-        "./images/IMG_20230304_223952.jpg",
-        "./images/IMG_20230310_201024.jpg",
-        "./images/IMG_20230319_224208.jpg",
-        "./images/IMG_20230418_192844-01.jpeg.jpg",
-        "./images/IMG_20230419_171148.jpg",
-        "./images/IMG_20230423_204344.jpg",
-        "./images/IMG_20230426_204232.jpg",
-        "./images/IMG_20230426_204253.jpg",
-        "./images/IMG_20230501_153611.jpg",
-        "./images/IMG_20230504_155454.jpg",
-        "./images/IMG_20230508_230400.jpg",
-        "./images/IMG_20230508_230643.jpg",
-        "./images/IMG_20230510_170449.jpg",
-        "./images/IMG_20230512_132447.jpg",
-        "./images/IMG_20230512_191520.jpg",
-        "./images/IMG_20230512_201301.jpg",
-        "./images/IMG_20230514_165551.jpg",
-        "./images/IMG_20230514_165625.jpg",
-        "./images/IMG_20230514_180117.jpg",
-        "./images/IMG_20230515_120337.jpg",
-        "./images/IMG_20230515_120353.jpg",
-        "./images/IMG_20230523_192153.jpg",
-        "./images/IMG_20230524_164929.jpg",
-        "./images/IMG_20230602_204642.jpg",
-        "./images/IMG_20230603_181418.jpg",
-        "./images/IMG_20230603_181613.jpg",
-        "./images/IMG_20230605_161417.jpg",
-        "./images/IMG_20230606_174314.jpg",
-        "./images/IMG_20230610_194624.jpg",
-        "./images/IMG_20230718_230327.jpg",
-        "./images/IMG_20230730_163238.jpg",
-        "./images/IMG_20230731_212336.jpg",
-        "./images/IMG_20230803_003754.jpg",
-        "./images/IMG_20230806_222041.jpg",
-        "./images/IMG_20230810_235335.jpg",
-        "./images/IMG_20231020_210701.jpg",
-        "./images/IMG_20221130_194825.jpg",
-        "./images/IMG_20221225_172908.jpg",
-        "./images/IMG_20221007_182651.jpg",
-        "./images/IMG_20221202_000045.jpg",
+        "images/w.jpg",
+        "images/pic9.jpg",
+        "images/pic8.jpg",
+        "images/pic6.jpg",
+        "images/pic5.jpg",
+        "images/pic5.2.jpg",
+        "images/pic4.jpg",
+        "images/pic3.1.jpg",
+        "images/pic2.jpg",
+        "images/pic1.jpg",
+        "images/pic0.jpg",
+        "images/pic0.1.jpg",
+        "images/pic-1.1.jpg",
+        "images/pic_1.jpg",
+        "images/pic 7.jpg",
+        "images/pic 3.2.jpg",
+        "images/IMG_20231208_133920-01.jpg",
+        "images/IMG_20230806_222041.jpg",
+        "images/IMG_20230606_174443-01.jpg",
+        "images/IMG_20230603_181619-01.jpg",
+        "images/IMG_20230508_230643-01---Copy.jpg",
+        "images/IMG_20230108_173113-01---Copy.jpg",
+        "images/IMG_20230107_223509-01---Copy.jpg",
+        "images/IMG_20230107_223438-01---Copy.jpg",
+        "images/IMG_20221227_161954-01---Copy.jpg",
+        "images/IMG_20221227_161949-01---Copy.jpg",
+        "images/IMG_20221225_172946-01---Copy.jpg",
+        "images/IMG_20221225_061805-01---Copy.jpg",
+        "images/IMG_2275-01.jpg",
+        "images/20220624_123447.jpg",
+        "images/20220604_224036.jpg",
+        "images/20210830_165751.jpg",
+        "images/20210310_185127.jpg",
+        "images/20210302_160019.jpg",
+        "images/20210207_171852.jpg",
+        "images/20210104_200602.jpg",
+        "images/20201220_202412.jpg",
+        "images/20201122_142508.jpg",
+        "images/20201027_162552.jpg"
         
     ];
 
     imagePaths.forEach((path, index) => {
         const thumbnail = document.createElement("div");
-        thumbnail.className = "col-md-4 col-6 mb-4"; // Adjusted class for mobile view
+        thumbnail.className = "col-md-3 col-6 mb-4"; // Updated class for 4 columns per row
         thumbnail.innerHTML = `
             <img src="${path}" alt="Image ${index + 1}" class="img-fluid thumbnail " data-index="${index}">
         `;
         galleryContainer.appendChild(thumbnail);
     });
-
     const thumbnails = document.querySelectorAll(".thumbnail");
     thumbnails.forEach(thumbnail => {
         thumbnail.addEventListener("click", displayImage);
@@ -116,16 +62,16 @@ document.addEventListener("DOMContentLoaded", function () {
         const imagePath = imagePaths[index];
     
         const modalContent = `
-            <div class="modal-content rounded-5">
+        <div class="modal-content rounded-5">
             <div class="modal-body bg-dark rounded-4 p-2">
-            <img src="${imagePath}" alt="Image ${parseInt(index) + 1}" class="img-fluid rounded-3" id="modalImage">
+                <img src="${imagePath}" alt="Image ${parseInt(index) + 1}" class="img-fluid rounded-3" id="modalImage">
             </div>
             <div class="modal-icons px-3">
-            <a href="${imagePath}" download="Image_${parseInt(index) + 1}.jpg" class="btn downloadd"><i class="fa-solid fa-circle-down" style="color: #e3e3e3;"></i></a>
-            <button type="button" class="btn downloadd" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-xmark" style="color: #ffffff;"></i></button>
-                </div>
+                <button type="button" class="btn downloadd" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-xmark" style="color: #ffffff;"></i></button>
             </div>
-        `;
+        </div>
+    `;
+    
 
         const modalContainer = document.createElement("div");
         modalContainer.classList.add("modal", "fade");
@@ -153,33 +99,33 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
 // page shifting ===========================================================
-function aboutBtn(){
+// function aboutBtn(){
 
-    const divelemnett = document.getElementById("about");
-          divelemnett.innerHTML = `
-            <object type="text/html" data="about.html" width="100%" height="500px"></object>
+//     const divelemnett = document.getElementById("about");
+//           divelemnett.innerHTML = `
+//             <object type="text/html" data="about.html" width="100%" height="500px"></object>
             
-            `;
-            const objectTag = divelemnett.querySelector('object');
-  objectTag.addEventListener('load', function () {
-    const contentDocument = objectTag.contentDocument;
-    contentDocument.body.style.overflow = 'hidden';
-  });
+//             `;
+//             const objectTag = divelemnett.querySelector('object');
+//   objectTag.addEventListener('load', function () {
+//     const contentDocument = objectTag.contentDocument;
+//     contentDocument.body.style.overflow = 'hidden';
+//   });
           
   
  
-  }
+//   }
 
-  const aboutElement = document.getElementById("about");
-  let originalContent = aboutElement.innerHTML;
+//   const aboutElement = document.getElementById("about");
+//   let originalContent = aboutElement.innerHTML;
 
-  function aboutBtn() {
-    aboutElement.innerHTML = ` <object type="text/html" data="about.html" width="100%" height="700px"></object>`;
-  }
+//   function aboutBtn() {
+//     aboutElement.innerHTML = ` <object type="text/html" data="about.html" width="100%" height="700px"></object>`;
+//   }
 
-  function showNormalPage() {
-    location.reload(true)
-  }
+//   function showNormalPage() {
+//     location.reload(true)
+//   }
 
 
 //   message sending and custom alert message =========================================
